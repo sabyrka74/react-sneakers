84a30cb34dfe7102c138fbf55471f2dc85fa90a8
Sabyrka <sabyrka74@gmail.com>
2026-05-23 06:09:56 +0500

    Update eslint.config.js
    Co-authored-by: Sabyrzhan sabyrzhasulanuly@gmail.com

--- eslint.config.js
+++ eslint.config.js
@@ -5,10 +5,6 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-asdasdssad
-
-
-
 export default defineConfig([
   globalIgnores(['dist']),
   {
